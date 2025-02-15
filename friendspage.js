09ed8fce12205d838d7da6afdc8812d7cb48aa36
tallaphid.1 <arjuns64rsv@gmail.com>
2025-02-15 12:25:11 +0630

    log in trial

--- friendspage.js
+++ friendspage.js
@@ -1,5 +1,6 @@
 
+function codeGenerate() {
+const friendCode = generateAlphaNumericCode(10); // Generates a code of length 10
+document.getElementById('displayCode').textContent = friendCode; }
 
-document.getElementById('generateCodeBtn').addEventListener('click', function() {
-const code = generateAlphaNumericCode(10); // Generates a code of length 10
-document.getElementById('displayCode').textContent = code;
+document.getElementById('codeButton').addEventListener('click', codeGenerate());
