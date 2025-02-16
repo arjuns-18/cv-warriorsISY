7e9ff684e6c3ccb5884664d4127005b29778f800
ayarwady <27lua@isyedu.org>
2025-02-16 09:45:58 +0630

    keep yourself safe

--- mainPageScript.js
+++ mainPageScript.js
@@ -1,13 +1,19 @@
 let clickCount = 0;
+const openButton = document.getElementById('openMindfulness');
+const modal = document.getElementById('mindfulnessModal');
+const closeModal = document.getElementById('closeModal');
 
-document.getElementById('openMindfulness').addEventListener('click', function(){
-
+// Toggle modal visibility
+openButton.addEventListener('click', function () {
     clickCount++;
-        if(clickCount%2 === 0) {
-            document.getElementById('mindfulnessModal').style.display = "none";
-        }
-        else if(clickCount%2 === 1) {
-            document.getElementById('mindfulnessModal').style.display = "flex";
-        }
-} )
+    if (clickCount % 2 === 0) {
+        modal.style.display = "none";
+    } else {
+        modal.style.display = "flex";
+    }
+});
 
+// Close the modal when the close button is clicked
+closeModal.addEventListener('click', function () {
+    modal.style.display = "none";
+});
