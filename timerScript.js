ef9469b5a34845bac4cf49b44b93e0eb260fae95
ifjofi <27jeongh@isyedu.org>
2025-02-16 12:51:20 +0630

    eggbreak sound

--- timerScript.js
+++ timerScript.js
@@ -2,6 +2,7 @@ let timerInterval;
 let time;
 let originalTime;
 let eggCrack = new Audio("sounds/eggcrack.mp3");
+let eggBreak = new Audio("sounds/eggbreak.mp3");
 let isTimerActive = false;  
 let fishAnimationInterval;
 let index = 1;
@@ -103,7 +104,6 @@ function animateFishAfterTimeUp() {
   if (randomNumber === 1) {
     fishAnimationInterval = setInterval(() => {
 
-      //dont mess with this - arjun
       if (!animalsUnlocked.includes("anglerfish")) { 
         animalsUnlocked.push("anglerfish");
         localStorage.setItem("animalsUnlocked", JSON.stringify(animalsUnlocked));
@@ -180,6 +180,7 @@ function stopTimerAndBreakEgg() {
 document.addEventListener('visibilitychange', function () {
   if (document.hidden && isTimerActive) {
     stopTimerAndBreakEgg();
+    eggBreak.play();
   }
 });
 
